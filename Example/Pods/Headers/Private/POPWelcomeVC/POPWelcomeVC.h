349f2fe23d81746ebdef1c3fea032c25031aee2c
popeveryday <popeveryday@gmail.com>
2016-04-04 22:02:34 +0700

    0.1.13

---
+++ Example/Pods/Headers/Private/POPWelcomeVC/POPWelcomeVC.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/POPWelcomeVC.h
--- Example/Pods/Target Support Files/POPWelcomeVC/POPWelcomeVC-umbrella.h
+++ Example/Pods/Target Support Files/POPWelcomeVC/POPWelcomeVC-umbrella.h
@@ -1,6 +1,7 @@
 #import <UIKit/UIKit.h>
 
 #import "AppWelcomeVC.h"
+#import "POPWelcomeVC.h"
 
 FOUNDATION_EXPORT double POPWelcomeVCVersionNumber;
 FOUNDATION_EXPORT const unsigned char POPWelcomeVCVersionString[];
